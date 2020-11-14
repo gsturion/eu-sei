b0b867affe665f3651ba8eb2f08b76ea99a6c357
roberto oliveira <roberto.c.o@gmail.com>
2020-11-14 11:29:12 -0300

    add_references

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,3 @@
 class Classroom < ApplicationRecord
+  has_many :users, :questions
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  has_many :alternatives, dependent: :destroy
+  belongs_to :classroom
 end
