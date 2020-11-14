12c1ba8abd02a3638e016f402d19e79500ee2f3b
Joao Helio <joaohelio@hotmail.com>
2020-11-14 11:57:21 -0300

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  resources :classrooms, only: [ :show ] do
+    resources :questions, only: [ :new, :create, :edit, :show, :delete ] do
+      resources :alternatives, only: [ :edit, :delete ]
+    end
+  end
+
+  patch "questions/:id/send", to: "questions#send", as: :send_question
+  get "classrooms/:id/dashboard", to: "classrooms#dashboard", as: :dashboard
+  get "classrooms/:id/feed", to: "classrooms#feed", as: :feed
+  post "/alternatives/:id/answers/create", to: "alternatives#create", as: :create_answer
 end
