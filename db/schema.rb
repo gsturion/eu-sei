0a4cd5489b52b91c428ee2c95b5005292709ea18
Joao Helio <joaohelio@hotmail.com>
2020-12-03 23:45:18 -0300

    chatroom working, need to work on routes and private chats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_181043) do
+ActiveRecord::Schema.define(version: 2020_12_04_020448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,11 +33,27 @@ ActiveRecord::Schema.define(version: 2020_11_14_181043) do
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
     t.integer "year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -72,6 +88,8 @@ ActiveRecord::Schema.define(version: 2020_11_14_181043) do
   add_foreign_key "alternatives", "questions"
   add_foreign_key "answers", "alternatives"
   add_foreign_key "answers", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "questions", "classrooms"
   add_foreign_key "questions", "users"
   add_foreign_key "users", "classrooms"
